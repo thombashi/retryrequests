e97c292f58fd9a626b4e73f792dce02c949f0a09
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-04-10 00:46:33 +0900

    Add type annotations

--- retryrequests/__init__.py
+++ retryrequests/__init__.py
@@ -2,15 +2,19 @@
 .. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
 """
 
+from typing import Any, Dict, Optional, Sequence
+
 import requests
-from requests import Session
+from requests import Response, Session
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 from .__version__ import __author__, __copyright__, __email__, __license__, __version__
 
 
-def make_requests_session(retries=5, backoff_factor=0.5, status_forcelist=(500, 502, 504)):
+def make_requests_session(
+    retries: int = 5, backoff_factor: float = 0.5, status_forcelist: Sequence[int] = (500, 502, 504)
+) -> Session:
     session = Session()
     adapter = HTTPAdapter(
         max_retries=Retry(
@@ -27,25 +31,25 @@ def make_requests_session(retries=5, backoff_factor=0.5, status_forcelist=(500, 
     return session
 
 
-def get(url, params=None, **kwargs):
+def get(url: str, params: Optional[Dict[str, Any]] = None, **kwargs) -> Response:
     return make_requests_session().get(url, params=params, **kwargs)
 
 
-def head(url, **kwargs):
+def head(url: str, **kwargs) -> Response:
     return make_requests_session().head(url, **kwargs)
 
 
-def post(url, data=None, json=None, **kwargs):
+def post(url: str, data=None, json: Optional[Dict[str, Any]] = None, **kwargs) -> Response:
     return make_requests_session().post(url, data=data, json=json, **kwargs)
 
 
-def put(url, data=None, **kwargs):
+def put(url: str, data=None, **kwargs) -> Response:
     return make_requests_session().put(url, data=data, **kwargs)
 
 
-def patch(url, data=None, **kwargs):
+def patch(url: str, data=None, **kwargs) -> Response:
     return make_requests_session().patch(url, data=data, **kwargs)
 
 
-def delete(url, **kwargs):
+def delete(url: str, **kwargs) -> Response:
     return make_requests_session().delete(url, **kwargs)
