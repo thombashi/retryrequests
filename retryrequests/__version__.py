318d179af18ffa5b9e359091d243fbac571e30c2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2022-03-06 17:42:40 +0900

    Modify copyright

--- retryrequests/__version__.py
+++ retryrequests/__version__.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-
 __author__ = "Tsuyoshi Hombashi"
-__copyright__ = "Copyright {}, {}".format(datetime.now().year, __author__)
+__copyright__ = f"Copyright 2019, {__author__}"
 __license__ = "MIT License"
 __version__ = "0.1.0"
 __maintainer__ = __author__
